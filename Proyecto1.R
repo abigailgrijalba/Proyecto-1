7276d3fdecdc35686073cd3a9fb339dd83e01c3f
abigailgrijalba <82826848+abigailgrijalba@users.noreply.github.com>
2021-06-13 16:36:15 -0600

    Commit inicial

---
+++ Proyecto1.R
@@ -0,0 +1,254 @@
+# Datos climáticos
+
+# Cargar los paquetes 
+library(dplyr)
+library(ggplot2)
+library(hrbrthemes)
+library(grid)
+library(gridExtra)
+
+# Se carga el archivo csv con el que se va a trabajar y se fija el directorio con setwd para tener los datos disponibles todo el tiempo
+setwd("C:/Users/abbyg/Desktop/Procesamiento de datos geográficos")
+datos <- read.csv("liberia_datos_climaticos.csv", na.strings = "", sep = ",", dec = ",")
+
+# Ahora bien, con el comando  head y dim se van ver los datos generales de el archivo como los encabezados, las dimensiones y el tipo de archivo que es un data.frame
+head(datos)
+dim(datos)
+class(datos)
+
+# La siguiente es una función mediante la cual se va indagar de manera óptima dentro del archivo csv ya que este posee NA
+datos[!complete.cases(datos),]
+
+# Se limpiará el archivo de NA con la función na.omit
+datos <- na.omit(datos)
+
+# Para un mayor manejo de la información y del trabajo se van a nombrar los encabezados de las columnas
+names(datos) <- c("Fecha","Temperatura", "Humedad", "Vel.Viento", "Lluvia", "Irradiacion", "EvapoTranspiracion")
+
+head(datos)
+# A continuación, se generarán los histogramas de cada variable
+hist1 <- ggplot(datos, aes(x = Temperatura)) +
+  geom_histogram(binwidth = 0.5,
+                 col = 'black',
+                 fill = 'red') +
+  ggtitle('Temperatura en Liberia 2015-2019') +
+  xlab('Grados Celcius') +
+  ylab('Frecuencia') +
+  theme_ipsum()
+
+hist2 <- ggplot(datos, aes(x = Humedad)) +
+  geom_histogram(binwidth = 2,
+                 col = 'black',
+                 fill = '#009900') +
+  ggtitle('Humedad relativa en Liberia 2015-2019') +
+  xlab('% de humedad relativa') +
+  ylab('Frecuencia') +
+  theme_ipsum()
+
+hist3 <- ggplot(datos, aes(x = Vel.Viento)) +
+  geom_histogram(binwidth = 1,
+                 col = 'black',
+                 fill = '#ff6600') +
+  ggtitle('Velocidad del viento en Liberia 2015-2019') +
+  xlab('Velocidad en m/s') +
+  ylab('Frecuencia') +
+  theme_ipsum()
+
+hist4 <- ggplot(datos, aes(x = Lluvia)) +
+  geom_histogram(binwidth = 5,
+                 col = 'black',
+                 fill = '#339999') +
+  ggtitle('Cantidad de lluvia en Liberia 2015-2019') +
+  xlab('Cant. de lluvia en mm') +
+  ylab('Frecuencia') +
+  theme_ipsum()
+
+hist5 <- ggplot(datos, aes(x = Irradiacion)) +
+  geom_histogram(binwidth = 7,
+                 col = 'black',
+                 fill = 'purple') +
+  ggtitle('Niveles de irradiación en Liberia 2015-2019') +
+  xlab('Irradiación en W/m2') +
+  ylab('Frecuencia') +
+  theme_ipsum()
+  
+hist6 <- ggplot(datos, aes(x = EvapoTranspiracion)) +
+  geom_histogram(binwidth = 1,
+                 col = 'black',
+                 fill = 'gray') +
+  ggtitle('Cantidad de EvapoTranspiración en Liberia 2015-2019') +
+  xlab('Cant. de EvapoTranspiración en mm') +
+  ylab('Frecuencia') +
+  theme_ipsum()
+
+# Una vez graficados los histogramas de cada variable se van a proyectar en una sola imágen con la función grind.arrange
+grid.arrange(hist1, hist2, hist3, hist4, hist5, hist6,
+             top = textGrob(
+               gp = gpar(fontface = 2, fontsize = 23),
+               "HISTOGRAMAS DE DATOS CLIMÁTICOS DE LA ESTACIÓN DE LIBERIA 2015-2019"))
+
+# Posterior a esto, mediante las funciones select(), mutate(), group_by() y summarise() se va a promediar mensualmente cada variable y la suma de la lluvia y la EvapoTranspiración, cabe recalcar que estas últimas dos se encuentran en mm, además, se crearán nuevos data.frame para cada variable y luego comenzar a graficar
+Temperatura_mensual <-
+  datos %>%
+  select(Fecha, Temperatura) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(mes = format(Fecha, "%m")) %>%
+  summarise(prom_temp = mean(Temperatura))
+
+Humedad_mensual <-
+  datos %>%
+  select(Fecha, Humedad) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(mes = format(Fecha, "%m")) %>%
+  summarise(prom_hum = mean(Humedad))
+
+Vel.Viento_mensual <-
+  datos %>%
+  select(Fecha, Vel.Viento) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(mes = format(Fecha, "%m")) %>%
+  summarise(prom_viento = mean(Vel.Viento))
+
+Irradiacion_mensual <-
+  datos %>%
+  select(Fecha, Irradiacion) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(mes = format(Fecha, "%m")) %>%
+  summarise(prom_irrad = mean(Irradiacion))
+
+Lluvia_mensual <- 
+  datos %>%
+  select(Fecha, Lluvia) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(mes = format(Fecha, "%m")) %>%
+  summarise(sum_lluvia = sum(Lluvia))
+
+EvapoTranpiracion_mensual <- 
+  datos %>%
+  select(Fecha, EvapoTranspiracion) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(mes = format(Fecha, "%m")) %>%
+  summarise(sum_evapo = sum(EvapoTranspiracion))
+
+# Ahora bien, tras haber promediado por mes cada variable y creado sus respectivos data.frame, se van a crear los gráficos de estos
+grafico1 <- ggplot(Temperatura_mensual, aes(x = mes, y = prom_temp, group = 1)) +
+  ggtitle('Promedio de temperatura mensual en Liberia 2015-2019') +
+  xlab('Meses') +
+  ylab('Grados Celsius') +
+  geom_point(colour = "red", size = 2) + 
+  geom_line(colour = "red", size  = 1)
+
+grafico2 <- ggplot(Humedad_mensual, aes(x = mes, y = prom_hum, group = 1)) +
+  ggtitle('Promedio de porcentaje de humedad mensual en Liberia 2015-2019') +
+  xlab('Meses') +
+  ylab('Porcentaje de humedad relativa') +
+  geom_point(colour = "#009900", size = 2) + 
+  geom_line(colour = "#009900", size = 1)
+
+grafico3 <- ggplot(Vel.Viento_mensual, aes(x = mes, y = prom_viento, group = 1)) +
+  ggtitle('Promedio de velocidad del viento mensual en Liberia 2015-2019') +
+  xlab('Meses') +
+  ylab('Velocidad del viento (m/s)') +
+  geom_point(colour = "#ff6600", size = 2) + 
+  geom_line(colour = "#ff6600", size = 1)
+
+grafico4 <- ggplot(Irradiacion_mensual, aes(x = mes, y = prom_irrad, group = 1)) +
+  ggtitle('Promedio de niveles de irradiación mensual en Liberia 2015-2019') +
+  xlab('Meses') +
+  ylab('Nivel de irradiación (w/m2)') +
+  geom_point(colour = "purple", size = 2) + 
+  geom_line(colour = "purple", size = 1)
+
+grafico5 <- ggplot(Lluvia_mensual, aes(x = mes, y = sum_lluvia, group = 1)) +
+  ggtitle('Total mensual de la Lluvia en Liberia 2015-2019') +
+  xlab('Meses') +
+  ylab('Cantidad de Lluvia (mm)') +
+  geom_point(colour = "#339999", size = 2) + 
+  geom_line(colour = "#339999", size = 1) 
+
+grafico6 <- ggplot(EvapoTranpiracion_mensual, aes(x = mes, y = sum_evapo, group = 1)) +
+  ggtitle('Total mensual de EvapoTranspiración en Liberia 2015-2019') +
+  xlab('Meses') +
+  ylab('Cantidad de EvapoTranspiración (mm)') +
+  geom_point(colour = "#cc9900", size = 2) + 
+  geom_line(colour = "#cc9900", size = 1) 
+  
+# Ya que se han creado los gráficos se van a unificar en el mismo grafico con la funcion grid.arrange
+grid.arrange(grafico1, grafico2, grafico3, grafico4, grafico5, grafico6,
+             top = textGrob(
+               gp = gpar(fontface = 2, fontsize = 23),
+               "PROMEDIOS MENSUALES DE DATOS CLIMÁTICOS DE LA ESTACIÓN DE LIBERIA 2015-2019"))
+
+# Finalmente, mediante una nube de puntos vamos a comparar información de algunas variables entre si, para empezar se va a crear un data.frame nuevo que contenga la información promediada de cada mes de los cuatro años, es decir, el promedio mensual de 51 meses desde enero 2015 hasta marzo 2019.
+Data_anual <- 
+  datos %>%
+  select(Fecha, Temperatura, Humedad, Vel.Viento, Lluvia, Irradiacion, EvapoTranspiracion) %>%
+  mutate(Fecha = as.Date(Fecha, format = "%d/%m/%Y")) %>%
+  group_by(meses = format(Fecha, "%m/%Y")) %>%
+  summarise(temp_anual = mean(Temperatura),
+            hume_anual = mean(Humedad),
+            viento_anual = mean(Vel.Viento),
+            lluv_anual = mean(Lluvia),
+            irrad_anual = mean(Irradiacion),
+            evapo_anual = mean(EvapoTranspiracion))
+
+# Una vez creados el data.frame, vamos a graficar entre sí algunas variables como Humedad y Temperatura o Lluvia y Humedad y a cada gráfico asignarle un nombre para facilitar el uso de la función grid.arrange
+point1 <- ggplot(Data_anual, aes(x = evapo_anual, y = lluv_anual)) +
+  geom_point(colour = 'blue') +
+  ggtitle('Relación entre la Lluvia y la EvapoTranspiración en Liberia 2015-2019') +
+  xlab('EvapoTranspiración') +
+  ylab('Lluvia') +
+  theme_ipsum(
+    grid = "Y, y"
+  )
+
+point2 <- ggplot(Data_anual, aes(x = hume_anual, y = temp_anual)) +
+  geom_point(colour = '#009900') +
+  ggtitle('Relación entre la Humedad y la Temperatura en Liberia 2015-2019') +
+  xlab('Humedad') +
+  ylab('Temperatura') +
+  theme_ipsum(
+    grid = "Y, y"
+  )
+
+point3 <- ggplot(Data_anual, aes(x = evapo_anual, y = hume_anual)) +
+  geom_point(colour = '#cc0066') +
+  ggtitle('Relación entre la Humedad y la EvapoTranspiración en Liberia 2015-2019') +
+  xlab('EvapoTranspiración') +
+  ylab('Humedad') +
+  theme_ipsum(
+    grid = "Y, y"
+  )
+
+point4 <- ggplot(Data_anual, aes(x = temp_anual, y = irrad_anual)) +
+  geom_point(colour = '#cc6600') +
+  ggtitle('Relación entre la Temperatura y la Irradiación en Liberia 2015-2019') +
+  xlab('Temperatura') +
+  ylab('Irradiación') +
+  theme_ipsum(
+    grid = "Y, y"
+  )
+
+point5 <- ggplot(Data_anual, aes(x = hume_anual, y = viento_anual)) +
+  geom_point(colour = '#00cccc') +
+  ggtitle('Relación entre la Humedad y la Velocidad del Viento en Liberia 2015-2019') +
+  xlab('Humedad') +
+  ylab('Velocidad del Viento') +
+  theme_ipsum(
+    grid = "Y, y"
+  )
+
+point6 <- ggplot(Data_anual, aes(x = hume_anual, y = lluv_anual)) +
+  geom_point(colour = '#cc00ff') +
+  ggtitle('Relación entre la Humedad y la Lluvia en Liberia 2015-2019') +
+  xlab('Humedad') +
+  ylab('Lluvia') +
+  theme_ipsum(
+    grid = "Y, y"
+  )
+
+# Finalmente se van a unir las nubes de puntos con la funcion grid.arrange
+grid.arrange(point1, point2, point3, point4, point5, point6,
+             top = textGrob(
+               gp = gpar(fontface = 2, fontsize = 23),
+               "NUBES DE PUNTOS DE LA RELACIÓN DE DATOS CLIMÁTICOS DE LA ESTACIÓN DE LIBERIA 2015-2019"))
